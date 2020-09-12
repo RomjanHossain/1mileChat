5e9104fd4f21968eb8776ce30dd02e57c7760ce4
RomjanHossain <romjanhossain726526@gmail.com>
2020-09-12 23:44:13 +0600

    multi provider added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:one_mile_chat/screens/chats.dart';
 import 'package:one_mile_chat/screens/homePage.dart';
+import 'package:one_mile_chat/services/auth.dart';
+import 'package:provider/provider.dart';
 
 import 'screens/StartPage.dart';
 
@@ -19,22 +22,29 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        appBarTheme: AppBarTheme(
-          color: Colors.white,
-          elevation: 1,
+    return MultiProvider(
+      providers: [
+        StreamProvider<User>.value(
+          value: AuthServices().user,
         ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          appBarTheme: AppBarTheme(
+            color: Colors.white,
+            elevation: 1,
+          ),
+        ),
+        initialRoute: StartPage.id,
+        routes: {
+          StartPage.id: (context) => StartPage(),
+          MyChatScreen.id: (context) => MyChatScreen(),
+          ChatScreen.id: (context) => ChatScreen(),
+        },
       ),
-      initialRoute: StartPage.id,
-      routes: {
-        StartPage.id: (context) => StartPage(),
-        MyChatScreen.id: (context) => MyChatScreen(),
-        ChatScreen.id: (context) => ChatScreen(),
-      },
     );
   }
 }
