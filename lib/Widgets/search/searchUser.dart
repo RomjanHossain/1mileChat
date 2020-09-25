f121bb5eb7495f12d005d4082f3fb0d3749ba2e7
RomjanHossain <romjanhossain726526@gmail.com>
2020-09-25 16:17:55 +0600

    searched function added

---
+++ lib/Widgets/search/searchUser.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SearchUser extends StatefulWidget {
+  @override
+  _SearchUserState createState() => _SearchUserState();
+}
+
+class _SearchUserState extends State<SearchUser> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
